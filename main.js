0aee29f636be06441f1cd3e27364a8d36d49c5ac
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-27 11:21:20 +0300

    Implement add book functionality

--- main.js
+++ main.js
@@ -2,3 +2,37 @@ const booksContainerEl = document.querySelector('.books-container');
 const titleEl = document.querySelector('#title');
 const authorEl = document.querySelector('#author');
 const btnAddEl = document.querySelector('.btn-submit');
+
+const books = [];
+
+function displayBooks() {
+  booksContainerEl.innerHTML = '';
+  books.forEach((book) => {
+    const builder = `
+      <div>
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+      </div>`;
+
+    booksContainerEl.insertAdjacentHTML('beforeend', builder);
+
+    const btnRemove = document.createElement('button');
+    btnRemove.textContent = 'Remove';
+
+    booksContainerEl.insertAdjacentElement('beforeend', btnRemove);
+
+    const lineBreak = document.createElement('hr');
+    booksContainerEl.insertAdjacentElement(lineBreak);
+  });
+}
+
+btnAddEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = titleEl.value;
+  const author = authorEl.value;
+
+  const book = { title, author };
+  books.push(book);
+
+  displayBooks();
+});
