388c9ca665ad63aeb24e11a8fb77429332605fcc
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-27 19:41:27 +0300

    Refactor code

--- main.js
+++ main.js
@@ -3,49 +3,49 @@ const titleEl = document.querySelector('#title');
 const authorEl = document.querySelector('#author');
 const btnAddEl = document.querySelector('.btn-submit');
 
-if (!localStorage.getItem('booksInfo')) {
-  localStorage.setItem('booksInfo', JSON.stringify([]));
-}
-const books = JSON.parse(localStorage.getItem('booksInfo'));
+let books;
+books = JSON.parse(localStorage.getItem('booksInfo')) || [];
 
-function uploadToStorage(books) {
-  localStorage.setItem('booksInfo', JSON.stringify(books));
-}
-
-/* eslint-disable no-use-before-define */
-function loadFromStorage() {
-  const books = JSON.parse(localStorage.getItem('booksInfo'));
-  displayBooks(books);
-}
-loadFromStorage();
-
-function removeBook() {
-  books.splice(this, 1);
-  uploadToStorage(books);
-  loadFromStorage();
+function uploadToStorage(book) {
+  localStorage.setItem('booksInfo', JSON.stringify(book));
 }
 
 function displayBooks() {
   booksContainerEl.innerHTML = '';
+  if (!books) return;
   books.forEach((book, index) => {
     const builder = `
-        <div>
-          <p>${book.title}</p>
-          <p>${book.author}</p>
-        </div>`;
+      <p>${book.title}</p>
+      <p>${book.author}</p>`;
 
-    booksContainerEl.insertAdjacentHTML('beforeend', builder);
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookDiv.insertAdjacentHTML('beforeend', builder);
 
     const btnRemove = document.createElement('button');
+    btnRemove.classList.add('remove');
+    btnRemove.setAttribute('id', index);
     btnRemove.textContent = 'Remove';
 
-    btnRemove.addEventListener('click', removeBook.bind(index));
-
-    booksContainerEl.insertAdjacentElement('beforeend', btnRemove);
+    bookDiv.insertAdjacentElement('beforeend', btnRemove);
 
     const lineBreak = document.createElement('hr');
+
+    booksContainerEl.appendChild(bookDiv);
     booksContainerEl.insertAdjacentElement('beforeend', lineBreak);
   });
+}
+
+function loadFromStorage() {
+  books = JSON.parse(localStorage.getItem('booksInfo')) || [];
+  displayBooks();
+}
+loadFromStorage();
+
+function removeBook(index) {
+  const modified = books.filter((book) => book !== books.at(index));
+  uploadToStorage(modified);
+  loadFromStorage();
 }
 
 btnAddEl.addEventListener('click', (e) => {
@@ -58,4 +58,11 @@ btnAddEl.addEventListener('click', (e) => {
 
   uploadToStorage(books);
   loadFromStorage();
+});
+
+booksContainerEl.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetId = +e.target.getAttribute('id');
+    removeBook(targetId);
+  }
 });
