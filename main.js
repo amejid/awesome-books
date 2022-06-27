d89248e4d647c8523e932f8192e2b8e06d400ec0
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-27 11:08:00 +0300

    Select elements in the JS

---
+++ main.js
@@ -0,0 +1,4 @@
+const booksContainerEl = document.querySelector('.books-container');
+const titleEl = document.querySelector('#title');
+const authorEl = document.querySelector('#author');
+const btnAddEl = document.querySelector('.btn-submit');
